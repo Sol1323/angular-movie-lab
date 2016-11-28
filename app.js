b698a4c794b99cf580595377df6947fe096dc768
Sol1323 <sbeniquez084@eap.edu>
2016-11-28 12:02:37 -0800

    movies display with ng repeat

---
+++ app.js
@@ -0,0 +1,38 @@
+
+angular
+  .module('movieApp', [])
+  .controller('MovieCtrl', MovieCtrl);
+
+  function MovieCtrl () {
+
+    var vm = this;
+
+    vm.moviesToWatch = [
+      {
+        id: 25,
+        title: 'The secret life of pets',
+        genre: 'Comedy'
+      },
+      {
+        id: 10,
+        title: 'Moana',
+        genre: 'Adventure'
+      },
+      {
+        id: 39,
+        title: 'Titanic',
+        genre: 'Drama'
+      },
+      {
+        id: 94,
+        title: 'Matilda',
+        genre: 'Comedy'
+      },
+      {
+        id: 143,
+        title: 'Sign',
+        genre: 'Thriller'
+      }
+    ];
+    console.log(vm);
+  };
